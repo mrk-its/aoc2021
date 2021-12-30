afc584f856aaca02cf6d01077e77d6267329c357
Mariusz Kryński <mrk@sed.pl>
2021-12-31 00:13:31 +0100

    port to mos-unknown-none target

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -1,12 +1,70 @@
-use fixedvec::Vec;
-fn main() {
+#![no_std]
+#![feature(start)]
+#![allow(unused_imports)]
+
+use arrayvec::ArrayVec;
+
+use core::panic::PanicInfo;
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    write("PANIC!!!");
+    loop {}
+}
+
+extern "C" {
+    fn __putchar(c: u8);
+}
+
+pub fn write(text: &str) {
+    text.bytes().for_each(|b| unsafe { __putchar(b) });
+}
+
+pub fn write_i16(value: i16) {
+    let digit = (value % 10);
+    let rem = value / 10;
+    if rem > 0 {
+        write_i16(rem);
+    }
+    unsafe {
+        __putchar(48 + (digit as u8));
+    }
+}
+
+#[start]
+fn main(_argc: isize, _argv: *const *const u8) -> isize {
     let input = include_str!("input.txt");
 
-    let values = input.split("\n").map(|i| i.parse::<isize>().unwrap()).collect::<Vec<_, 2000>>();
+    write("parsing...");
+    let parsed = input
+        .split('\n')
+        .map(|i| i.parse::<i16>().unwrap())
+        .collect::<ArrayVec<i16, 2000>>();
+    write("done\n");
 
-    let values = values.iter().zip(&values[1..]).zip(&values[2..]).map(|((a, b), c)| a + b + c).collect::<Vec<_, 2000>>();
+    let part1 = parsed
+        .iter()
+        .zip(&parsed[1..])
+        .filter(|(a, b)| a < b)
+        .count();
 
-    let result = values.iter().zip(&values[1..]).filter(|(a, b)| b > a).count();
+    write("part1: ");
+    write_i16(part1 as i16);
+    write("\n");
 
-    println!("result: {:?}", result);
+    let values = parsed
+        .iter()
+        .zip(&parsed[1..])
+        .zip(&parsed[2..])
+        .map(|((a, b), c)| a + b + c);
+    let part2 = values
+        .clone()
+        .zip(values.skip(1))
+        .filter(|(a, b)| b > a)
+        .count();
+    write("part2: ");
+    write_i16(part2 as i16);
+    write("\n");
+
+    loop {}
 }
