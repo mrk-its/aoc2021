2eac0e9c8f91204d42e28d463ac6abf306836386
Mariusz Kryński <mrk@sed.pl>
2022-01-17 07:13:31 +0000

    cargo fmt

--- day25/src/atari.rs
+++ day25/src/atari.rs
@@ -1,16 +1,12 @@
-use volatile_register::{RO, RW};
-
-use ufmt_write::uWrite;
-
-
-const DMACTL: usize = 0x22f;
-const DLPTRS: usize = 0x230;
-const TIMER: usize = 18;
+use a800xl_utils::{
+    clock, consts,
+    screen::{clear_atract, ScreenMemoryWriter},
+};
 
 #[derive(Clone, Copy, Default)]
 struct DisplayListLine {
     pub mode: u8,
-    pub addr: usize,
+    pub addr: u16,
 }
 
 #[repr(C, align(512))]
@@ -23,7 +19,7 @@ struct DisplayList {
 }
 
 impl DisplayList {
-    pub fn update(&mut self, memory_addr: usize, info_addr: usize) {
+    pub fn update(&mut self, memory_addr: u16, info_addr: u16) {
         let mut addr = memory_addr;
         for line in self.lines.iter_mut() {
             line.mode = 0x4e;
@@ -31,9 +27,12 @@ impl DisplayList {
             addr += 35;
         }
         self.info.addr = info_addr;
-        self.jmp.addr = self as *mut DisplayList as usize;
-        io_write(DLPTRS, self.jmp.addr);
-        io_write(DMACTL, 0x21u8);
+        self.jmp.addr = self as *mut DisplayList as u16;
+
+        unsafe {
+            *consts::SDLST = self.jmp.addr;
+            *consts::SDMCTL = 0x21;
+        }
     }
 }
 
@@ -55,22 +54,6 @@ impl Default for DisplayList {
     }
 }
 
-pub fn io_write<T: Copy>(addr: usize, value: T) {
-    unsafe {
-        (*(addr as *const RW<T>)).write(value);
-    }
-}
-
-pub fn io_read<T: Copy>(addr: usize) -> T {
-    unsafe { (*(addr as *const RO<T>)).read() }
-}
-
-pub fn clock() -> u32 {
-    io_read::<u8>(TIMER + 2) as u32
-        + io_read::<u8>(TIMER + 1) as u32 * 256
-        + io_read::<u8>(TIMER + 0) as u32 * 65536
-}
-
 #[derive(Default)]
 pub struct Display {
     info: [u8; 32],
@@ -78,41 +61,11 @@ pub struct Display {
     start_t: u32,
 }
 
-struct ScreenMemoryWriter<'a> {
-    buffer: &'a mut [u8],
-    written: usize,
-}
-
-impl<'a> ScreenMemoryWriter<'a> {
-    fn new(buffer: &'a mut [u8]) -> ScreenMemoryWriter<'a> {
-        ScreenMemoryWriter { buffer, written: 0 }
-    }
-}
-
-impl<'a> uWrite for ScreenMemoryWriter<'a> {
-    type Error = ();
-
-    fn write_str(&mut self, s: &str) -> Result<(), Self::Error> {
-        for b in s.bytes() {
-            if self.written >= self.buffer.len() {
-                return Err(())
-            }
-            self.buffer[self.written] = match b & 0x7f {
-                0..=31 => b + 64,
-                32..=95 => b - 32,
-                _ => b,
-            } | (b & 128);
-            self.written += 1;
-        }
-        Ok(())
-    }
-}
-
 impl Display {
     pub fn show(&mut self, board: &crate::Board, frame_cnt: usize, phase: u8) {
         self.dlist.update(
-            board as *const crate::Board as usize,
-            &self.info as *const [u8; 32] as usize,
+            board as *const crate::Board as u16,
+            &self.info as *const [u8; 32] as u16,
         );
         let t = clock();
         if self.start_t == 0 {
