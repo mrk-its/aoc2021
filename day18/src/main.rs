f41c00a9ad57c09dcd46714543bbd26d973d8235
Mariusz Kryński <mrk@sed.pl>
2022-05-23 19:38:37 +0000

    day18

--- day18/src/main.rs
+++ day18/src/main.rs
@@ -1,14 +1,26 @@
-use std::str::Chars;
+#![no_std]
+#![feature(start)]
+#![feature(default_alloc_error_handler)]
+
+utils::entry!(main);
+extern crate alloc;
+extern crate mos_alloc;
+use alloc::boxed::Box;
+use alloc::vec::Vec;
+
+use ufmt_stdio::*;
+
+type Int = u16;
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 enum SNum {
-    Regular(u32),
+    Regular(Int),
     Pair(Box<(SNum, SNum)>),
 }
 
 impl SNum {
     fn repl(&self, v: SNum) {
-        let mut_ref: &mut SNum = unsafe {std::mem::transmute( self as *const SNum)};
+        let mut_ref: &mut SNum = unsafe { core::mem::transmute(self as *const SNum) };
         *mut_ref = v;
     }
     fn pair(a: SNum, b: SNum) -> SNum {
@@ -26,26 +38,34 @@ impl SNum {
             }
             if result.split() {
                 // println!("splitted: {:?}", self);
-                continue
+                continue;
             }
-            break
+            break;
         }
         result
     }
     fn split(&self) -> bool {
         match self {
-            SNum::Regular(x) => if *x > 9 {
-                let a = *x / 2;
-                let b = *x - a;
-                self.repl(SNum::Pair(Box::new((SNum::Regular(a), SNum::Regular(b)))));
-                true
-            } else {
-                false
+            SNum::Regular(x) => {
+                if *x > 9 {
+                    let a = *x / 2;
+                    let b = *x - a;
+                    self.repl(SNum::Pair(Box::new((SNum::Regular(a), SNum::Regular(b)))));
+                    true
+                } else {
+                    false
+                }
             }
-            SNum::Pair(pair) => pair.0.split() || pair.1.split()
+            SNum::Pair(pair) => pair.0.split() || pair.1.split(),
         }
     }
-    fn _explode<'a>(&'a self, depth: i32, exploded: &mut Option<SNum>, left: &mut Option<&'a SNum>, right: &mut Option<&'a SNum>) {
+    fn _explode<'a>(
+        &'a self,
+        depth: i32,
+        exploded: &mut Option<SNum>,
+        left: &mut Option<&'a SNum>,
+        right: &mut Option<&'a SNum>,
+    ) {
         match self {
             SNum::Regular(x) => {
                 if exploded.is_none() {
@@ -54,17 +74,19 @@ impl SNum {
                     *right = Some(self)
                 }
             }
-            SNum::Pair(pair) => if depth == 0 && exploded.is_none() {
-                match &**pair {
-                    (SNum::Regular(_), SNum::Regular(_)) => {
-                        *exploded = Some(self.clone());
-                        self.repl(SNum::Regular(0));
+            SNum::Pair(pair) => {
+                if depth == 0 && exploded.is_none() {
+                    match &**pair {
+                        (SNum::Regular(_), SNum::Regular(_)) => {
+                            *exploded = Some(self.clone());
+                            self.repl(SNum::Regular(0));
+                        }
+                        _ => (),
                     }
-                    _ => (),
+                } else {
+                    pair.0._explode(depth - 1, exploded, left, right);
+                    pair.1._explode(depth - 1, exploded, left, right);
                 }
-            } else {
-                pair.0._explode(depth - 1, exploded, left, right);
-                pair.1._explode(depth - 1, exploded, left, right);
             }
         }
     }
@@ -92,39 +114,38 @@ impl SNum {
                 true
             }
             None => false,
-            _ => panic!("wrong exploded")
+            _ => panic!("wrong exploded"),
         }
     }
-    fn magnitude(&self) -> u32 {
+    fn magnitude(&self) -> Int {
         match self {
             SNum::Regular(x) => *x,
-            SNum::Pair(pair) => 3 * pair.0.magnitude() + 2 * pair.1.magnitude(),           
+            SNum::Pair(pair) => 3 * pair.0.magnitude() + 2 * pair.1.magnitude(),
         }
     }
 }
 
-fn parse_chars(input: &mut Chars) -> SNum {
-    let c = input.next().unwrap();
-    if c.is_digit(10) {
-        SNum::Regular(c.to_digit(10).unwrap())
-    } else if c == '[' {
-        let left = parse_chars(input);
-        assert_eq!(input.next().unwrap(), ',');
-        let right = parse_chars(input);
-        assert_eq!(input.next().unwrap(), ']');
+fn parse_line<'a>(input: &mut impl Iterator<Item = &'a u8>) -> SNum {
+    let c = *input.next().unwrap();
+    if c >= 48 && c < 48 + 10 {
+        SNum::Regular(c as Int - 48)
+    } else if c == b'[' {
+        let left = parse_line(input);
+        assert_eq!(*input.next().unwrap(), b',');
+        let right = parse_line(input);
+        assert_eq!(*input.next().unwrap(), b']');
         SNum::Pair(Box::new((left, right)))
     } else {
         panic!()
     }
 }
 
-fn parse(input: &str) -> SNum {
-    let mut iter = input.chars();
-    parse_chars(&mut iter)
-}
-
 fn main() {
-    let input = include_str!("input.txt").split("\n").map(|line| parse(line)).collect::<Vec<_>>();
+    mos_alloc::set_limit(16384);
+    let input = utils::iter_lines!("input.txt")
+        .map(|line| parse_line(&mut line.iter()))
+        .collect::<Vec<_>>();
+    println!("parsed!");
     let mut iter = input.iter();
     let mut result = iter.next().unwrap().clone();
 
@@ -138,12 +159,10 @@ fn main() {
         for (j, b) in input.iter().enumerate() {
             if i != j {
                 max = a.add(b).magnitude().max(max);
-
             }
         }
     }
     println!("part2: {}", max);
-
 }
 
 #[test]
@@ -169,19 +188,34 @@ fn test_split2() {
 fn test_split3() {
     let mut n = SNum::pair(SNum::Regular(10), SNum::Regular(10));
     assert!(n.split());
-    assert_eq!(n, SNum::pair(SNum::pair(SNum::Regular(5), SNum::Regular(5)), SNum::Regular(10)));
+    assert_eq!(
+        n,
+        SNum::pair(
+            SNum::pair(SNum::Regular(5), SNum::Regular(5)),
+            SNum::Regular(10)
+        )
+    );
 }
 
 #[test]
 fn test_split4() {
     let mut n = SNum::pair(SNum::Regular(1), SNum::Regular(10));
     assert!(n.split());
-    assert_eq!(n, SNum::pair(SNum::Regular(1), SNum::pair(SNum::Regular(5), SNum::Regular(5))));
+    assert_eq!(
+        n,
+        SNum::pair(
+            SNum::Regular(1),
+            SNum::pair(SNum::Regular(5), SNum::Regular(5))
+        )
+    );
 }
 
 #[test]
 fn test_magnitude() {
-    assert_eq!(parse("[[[[8,7],[7,7]],[[8,6],[7,7]]],[[[0,7],[6,6]],[8,7]]]").magnitude(), 3488)
+    assert_eq!(
+        parse("[[[[8,7],[7,7]],[[8,6],[7,7]]],[[[0,7],[6,6]],[8,7]]]").magnitude(),
+        3488
+    )
 }
 
 #[test]
@@ -206,7 +240,6 @@ fn test_explodable3() {
     assert!(expl);
     assert_eq!(n, parse("[[6,[5,[7,0]]],3]"));
 }
-
 
 #[test]
 fn test_explodable4() {
