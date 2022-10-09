96a4a6540d8303f58d0f7d67cb697ba50e8d76a8
Mariusz Kryński <mrk@sed.pl>
2022-10-09 23:07:34 +0200

    compilation hang fix (!!!)

--- day06/src/main.rs
+++ day06/src/main.rs
@@ -7,26 +7,11 @@ type BigInt = u64;
 
 type Population = [BigInt; 10];
 
-extern "C" {
-    fn __muldi3(a: u64, b: u64) -> u64;
-}
-
-// for now we have int module commented out on mos arch, so we don't have 128bit support
-// but for some reason this program requires 128 bit multiplication __multi3 intrinsic
-// and throws: ld.lld: error: undefined symbol: __multi3 during linking
-// so let's cheat a bit:
-
-#[no_mangle]
-pub unsafe fn __multi3(a: u128, b: u128) -> u128 {
-    return __muldi3(a as u64, b as u64) as u128;
-}
-
-// interesting thing - always returning zero from above function produces correct results
-// so it seems it is not used at all
 
 fn parse(data: &str) -> Population {
     let mut population: Population = Default::default();
-    for i in data.split(',').map(|v| v.parse::<BigInt>().unwrap()) {
+    // changing below to parse::<u64> hangs compilation forever!
+    for i in data.split(',').map(|v| v.parse::<i8>().unwrap()) {
         population[i as usize] += 1;
     }
     population
